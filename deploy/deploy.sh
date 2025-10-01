bf179bed181f47bd6ec620815d5bf4ea21c2fd6c
Brigham Andersen <brighambandersen@gmail.com>
2025-10-01 06:25:55 +0000

    Switch deployment to cp

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,7 @@ echo "Deploying photography"
 
 # nginx
 
-sudo ln -sf /home/brig/code/photography/deploy/nginx.conf /etc/nginx/conf.d/photography.conf
+sudo cp /home/brig/code/photography/deploy/nginx.conf /etc/nginx/conf.d/photography.conf
 
 sudo nginx -t
 sudo systemctl reload nginx
