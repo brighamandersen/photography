ec55c55fdd5b44cf9330862745f480773a677f17
Brigham Andersen <brighambandersen@gmail.com>
2026-06-05 23:54:36 -0600

    update dev references

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,7 @@ echo "Deploying photography"
 
 # nginx
 
-sudo cp /home/brig/code/photography/deploy/nginx.conf /etc/nginx/conf.d/photography.conf
+sudo cp /home/brig/dev/photography/deploy/nginx.conf /etc/nginx/conf.d/photography.conf
 
 sudo nginx -t
 sudo systemctl reload nginx
